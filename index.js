e950edca1c61afc823b750f2a6f4a9926c871387
thomas <tmeyerwebdev@gmail.com>
2021-09-25 01:44:46 +0000

    Init commit

--- index.js
+++ index.js
@@ -1,29 +1,66 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
+  }
+}
 
-  constructor(amount) {
+class Transaction {
+
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance += this.value;
   }
 
 }
 
+class Deposit extends Transaction {
 
+  get value() {
+    return this.amount;
+  }
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+}
 
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account("snow-patrol");
 
-t1 = new Withdrawal(50.25);
+console.log('Starting Balance', myAccount.balance);
+
+let t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+let t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+let t3 = new Deposit(120.00, myAccount);
+t3.commit();
+
+console.log('Transaction 3:', t3);
+
+
+
+console.log('Balance:', myAccount.balance);
+
+
+
+
+
+
+
+
